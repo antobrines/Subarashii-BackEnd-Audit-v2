8d639ecdec2d0133f3d5fcb17c16f09652cee5c8
Antonin BRINES <antonin.brines@outlook.fr>
2022-05-30 09:47:34 +0200

    feat: 🎸 Add roles and banned in user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -15,7 +15,17 @@ const userSchema = mongoose.Schema({
     type: types.String,
     required: true,
     unique: true,
-  }
+  },
+  roles: {
+    type: types.Array,
+    required: true,
+    default: ['user'],
+  },
+  banned: {
+    type: types.Boolean,
+    required: true,
+    default: false,
+  },
 });
 
 const User = mongoose.model('User', userSchema);
