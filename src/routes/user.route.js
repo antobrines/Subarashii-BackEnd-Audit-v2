088c48ac41669499a0c83336b1eb14cc900cb31d
Tim GIMENEZ <tim.gimenez@ynov.com>
2022-05-31 15:28:52 +0200

    refactor: 💡 change route url /me

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -14,7 +14,7 @@ router.post(
   userController.register
 );
 router.post('/login', validate(authValidation.login), userController.login);
-router.get('/test-connection', [isConnected, isBanned], userController.testConnection);
+router.get('/me', [isConnected, isBanned], userController.me);
 router.put('/password', [isConnected, validate(authValidation.updatePassword)], userController.updatePassword)
 
 router.post('/password/reset/key', validate(authValidation.generateResetPasswordKey), userController.generateResetPasswordKey)
