4dfe1b324cdfb1656acf633ed3497a09690b5998
Tim GIMENEZ <tim.gimenez@ynov.com>
2022-05-31 10:52:18 +0200

    feat: 🎸 reset password routes
    Creation of [POST]'/user/password/reset/key' and
    [POST]'/user/password/reset' routes to generate reset password key and
    reset password

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -17,5 +17,8 @@ router.post('/login', validate(authValidation.login), userController.login);
 router.get('/test-connection', [isConnected, isBanned], userController.testConnection);
 router.put('/password', [isConnected, validate(authValidation.updatePassword)], userController.updatePassword)
 
+router.post('/password/reset/key', validate(authValidation.generateResetPasswordKey), userController.generateResetPasswordKey)
+router.post('/password/reset/', validate(authValidation.resetPassword), userController.resetPassword)
+
 
 module.exports = router;
