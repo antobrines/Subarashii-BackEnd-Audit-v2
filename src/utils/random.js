27484d63aa36352bae28f3bad4931e9fbb399cec
Tim GIMENEZ <tim.gimenez@ynov.com>
2022-06-01 09:43:34 +0200

    refactor: 💡 fix security generate random string

--- src/utils/random.js
+++ src/utils/random.js
@@ -1,15 +1,7 @@
+const crypto = require('crypto');
+
 const randomString = (length = 8) => {
-  // Declare all characters
-  let chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-
-  // Pick characers randomly
-  let str = '';
-  for (let i = 0; i < length; i++) {
-    str += chars.charAt(Math.floor(Math.random() * chars.length));
-  }
-
-  return str;
-
+  return crypto.randomBytes(length).toString('hex');
 };
 
 module.exports = {
