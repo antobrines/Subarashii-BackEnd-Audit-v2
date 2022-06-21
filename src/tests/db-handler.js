20e137b5f2154896695f6b2f94a78cfa3c9f1a32
Antonin BRINES <antonin.brines@outlook.fr>
2022-06-21 09:16:04 +0200

    feat: 🎸 add fake mongodb database

---
+++ src/tests/db-handler.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const {
+  MongoMemoryServer
+} = require('mongodb-memory-server');
+
+
+const connect = async () => {
+  const mongo = await MongoMemoryServer.create();
+  const uri = mongo.getUri();
+
+  const mongooseOpts = {
+    useNewUrlParser: true,
+  };
+
+  await mongoose.connect(uri, mongooseOpts);
+}
+
+const closeDatabase = async () => {
+  const mongo = await MongoMemoryServer.create();
+  await mongoose.connection.dropDatabase();
+  await mongoose.connection.close();
+  await mongo.stop();
+}
+
+const clearDatabase = async () => {
+  const collections = mongoose.connection.collections;
+
+  for (const key in collections) {
+    const collection = collections[key];
+    await collection.deleteMany();
+  }
+}
+
+module.exports = {
+  connect,
+  closeDatabase,
+  clearDatabase
+}
